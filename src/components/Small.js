833aba691f352db31184a9379fda3ed7f51ad28a
esteloabell <manitobaaa@gamil.com>
2020-09-28 18:22:07 -0500

    added company

--- src/components/Small.js
+++ src/components/Small.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "../styles/main.scss";
 
+
 export default class Small extends Component {
   render() {
     return (
@@ -9,34 +10,44 @@ export default class Small extends Component {
           <div className="tab">
             <input type="radio" id="rd1" name="rd" />
             <label className="tab-label" for="rd1">
-              <div className="items-company-container">
-                <ul>
-                  <li>1</li>
-                  <li>logo</li>
-                  <li>Scribe Media</li>
-                </ul>
-                <ul>
-                  <li>Austin, Texas</li>
-                </ul>
-                <ul>
-                  <li>Publishing</li>
-                </ul>
-              </div>
+              <SmallNav
+                rank="1"
+                companylogo="LOGO"
+                companyname="Scribe Media"
+                companylocation="Austin, Texas"
+                companysector="Commercial Real Estate"
+              />
             </label>
-            <div className="tab-content">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, facilis.
-            </div>
+            <Smallcontent
+              summary="Scribe helps professionals share their knowledge by turning their ideas into professionally published books and speeches. Its clients include Fortune 500 CEOs and executives, doctors, lawyers, financial advisors, high level consultants and entrepreneurs."
+              location="Austin. Texas"
+              founded="2014"
+              sector="Publishing"
+              number="35"
+              website="https://scribemedia.com/"
+              imagesrc="https://companycultures.netlify.app/static/media/scribe.38b62f37.png"
+            />
           </div>
           <div className="tab">
             <input type="radio" id="rd2" name="rd" />
             <label className="tab-label" for="rd2">
-              <div>
-                <p>Item 2</p>
-              </div>
+              <SmallNav 
+                rank="2"
+                companylogo="LOGO"
+                companyname="Hughes Marino"
+                companylocation="San Diego, Calif."
+                companysector="Publishing"
+              />
             </label>
-            <div className="tab-content">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, aut.
-            </div>
+            <Smallcontent
+              summary="Hughes Marino is a real estate firm that exclusively represents tenants and buyers -- never landlords -- so it never has a conflict of interest. With offices across the nation, Hughes Marino has been helping companies lease, buy and build commercial space for over 25 years."
+              location="San Diego, Calif."
+              founded="2011"
+              sector="Commercial Real Estate"
+              number="74"
+              website="http://hughesmarino.com/"
+              imagesrc="https://companycultures.netlify.app/static/media/hughes.b5c7b380.jpg"
+            />
           </div>
           <div className="tab">
             <input type="radio" id="rd3" name="rd" />
@@ -45,9 +56,15 @@ export default class Small extends Component {
                 <p>Item 3</p>
               </div>
             </label>
-            <div className="tab-content">
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos, facilis.
-            </div>
+            <Smallcontent
+              summary=""
+              location=""
+              founded=""
+              sector=""
+              number=""
+              website=""
+              imagesrc=""
+            />
           </div>
           <div className="tab">
             <input type="radio" id="rd4" name="rd" />
@@ -56,13 +73,70 @@ export default class Small extends Component {
                 <p>Item 4</p>
               </div>
             </label>
-            <div className="tab-content">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil, aut.
-            </div>
+            <Smallcontent
+              summary=""
+              location=""
+              founded=""
+              sector=""
+              number=""
+              website=""
+              imagesrc=""
+            />
           </div>
         </div>
       </div>
     )
   }
+}
+
+function SmallNav(props) {
+  return (
+    <div>
+      <div className="items-company-container">
+        <ul>
+          <li>{props.rank}</li>
+          <li>{props.companylogo}</li>
+          <li>{props.companyname}</li>
+        </ul>
+        <ul>
+          <li>{props.companylocation}</li>
+        </ul>
+        <ul>
+          <li>{props.companysector}</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+function Smallcontent(props) {
+  return (
+    <div className="tab-content">
+      <div className="tab-content-inner">
+        <p>{props.summary}</p>
+      </div>
+      <div className="tab-content-company-info">
+        <div className="company-pic">
+          <img src={props.imagesrc} alt="" />
+        </div>
+        <div className="tab-ul-container">
+          <ul>
+            <li>Location</li>
+            <li>Year Founded</li>
+            <li>Sector</li>
+            <li># of U.S. Employees</li>
+            <li>Website</li>
+          </ul>
+          <ul>
+            <li>{props.location}</li>
+            <li>{props.founded}</li>
+            <li>{props.sector}</li>
+            <li>{props.number}</li>
+            <li>{props.website}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
 }
 
